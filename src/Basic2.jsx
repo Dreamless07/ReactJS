a0b8714a5943a368d927cafa3f24b49386bf1b05
Dreamless07 <saadkhan7903@gmail.com>
2025-09-03 10:21:52 +0530

    Added images

--- src/Basic2.jsx
+++ src/Basic2.jsx
@@ -2,6 +2,8 @@ function NewName(){
         return(
                 <div>
                         <h2>Lets continue</h2>
+                        <img src="https://st2.depositphotos.com/4541049/6792/i/450/depositphotos_67921777-stock-photo-man-sitting-on-bench-on.jpg"/>
+                        <img src="favicon.ico" height="400"/>{/*downloaded images should be in public directory*/}
                     </div>
             )
     }
