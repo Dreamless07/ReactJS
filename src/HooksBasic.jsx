609c71cd51403be737b4b70ee83c61affc2b495d
Dreamless07 <saadkhan7903@gmail.com>
2025-09-03 12:20:04 +0530

    Added hooks and commented all blocks

--- src/HooksBasic.jsx
+++ src/HooksBasic.jsx
@@ -1,9 +1,9 @@
-import {useState} from 'react';
+import {useState} from 'react';{/*Import usestate*/}
 const HookComp=()=>{
-    const [testing,setTesting]=useState("Works fine, I guess")
+    const [testing,setTesting]=useState("Works fine, I guess"){/*Inbuilt function useState*/}
         return(
                 <div>
-                    <h2>About Hooks/usestate, {testing}</h2>
+                    <h2>About Hooks/usestate, {testing}</h2>{/*Print it using variable*/}
                 </div>
             )
     }
