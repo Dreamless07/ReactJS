7759ed345c71cf52c9cbdeaf238d31f52d96a039
Dreamless07 <saadkhan7903@gmail.com>
2025-09-03 12:38:22 +0530

    Added hooks(useState) and useEffect, commented all blocks

--- src/HooksBasic.jsx
+++ src/HooksBasic.jsx
@@ -1,9 +1,11 @@
-import {useState} from 'react';{/*Import usestate*/}
+import {useState,useEffect} from 'react';//Import useState and useEffect
 const HookComp=()=>{
-    const [testing,setTesting]=useState("Works fine, I guess"){/*Inbuilt function useState*/}
+    const [testing,setTesting]=useState("Works fine, I guess");//*Inbuilt function useState
+    //useEffect(()=>{here will be the actual code},[an inbuilt array]) (for errors like infinite loops )
         return(
                 <div>
                     <h2>About Hooks/usestate, {testing}</h2>{/*Print it using variable*/}
+                    <button onClick={()=>setTesting("You just changed it")}> Click Here </button>{/*Modified through setTesting*/}
                 </div>
             )
     }
